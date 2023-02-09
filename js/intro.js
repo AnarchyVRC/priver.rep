6ab04b3fcf70a6aafca0c3f258fea3cfc865e7ac
Skullface/Tarkov <106857740+terkoshi@users.noreply.github.com>
2023-02-09 17:06:13 -0500

    :D

--- js/intro.js
+++ js/intro.js
@@ -1,24 +1,24 @@
 
 var introscreen = document.getElementById('sequencer')
-var formula = document.getElementById('dolam')
+var links = document.getElementById('links')
 introscreen.style.display = 'none';
-formula.style.display = 'none';
+links.style.display = 'none';
 function play()
 {
-    var priverA = new Rythm()
-
-      priverA.setMusic('/assets/ost/bg.mp3')
-
-
-      priverA.setGain(1.0);
-      priverA.start()
+  
     var introscreen = document.getElementById('sequencer')
     var links = document.getElementById('links')
     var preintro = document.getElementById('enter')
     links.style.display = 'block'
     introscreen.style.display = 'block';
     enter.remove();
-    
+
+    var priverA = new Rythm()
+
+    //audio + animation
+    priverA.setMusic('/assets/ost/bg.mp3')
+    priverA.setGain(1.0);
+    priverA.start()
 }
     
 
