c69d76bf99058a2845a0aac780b7b452fce70e3f
Skullface/Tarkov <106857740+terkoshi@users.noreply.github.com>
2023-02-09 16:50:39 -0500

    S

--- js/intro.js
+++ js/intro.js
@@ -1,17 +1,17 @@
 
 var introscreen = document.getElementById('sequencer')
 var formula = document.getElementById('dolam')
-
 introscreen.style.display = 'none';
 formula.style.display = 'none';
-
 function play()
 {
+    
     var introscreen = document.getElementById('sequencer')
     var formula = document.getElementById('dolam')
     var preintro = document.getElementById('enter')
-    formula.style.display = 'block'
+    formula.style.display = 'block';
     introscreen.style.display = 'block';
+    
 }
     
 
