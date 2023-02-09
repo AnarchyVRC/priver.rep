7e080a40efdbdf4a7d574b680a094aa1ed4f9084
Skullface/Tarkov <106857740+terkoshi@users.noreply.github.com>
2023-02-09 09:14:08 -0500

    bored

---
+++ js/intro.js
@@ -0,0 +1,19 @@
+
+var introscreen = document.getElementById('sequencer')
+var formula = document.getElementById('dolam')
+
+introscreen.style.display = 'none';
+formula.style.display = 'none';
+
+function play()
+{
+    var introscreen = document.getElementById('sequencer')
+    var formula = document.getElementById('dolam')
+    var preintro = document.getElementById('enter')
+    formula.style.display = 'block'
+    introscreen.style.display = 'block';
+}
+    
+
+    
+    
