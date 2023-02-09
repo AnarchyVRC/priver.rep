ca61c7b85d462f1a1d940339a2b325f031d6ec89
Skullface/Tarkov <106857740+terkoshi@users.noreply.github.com>
2023-02-09 16:58:32 -0500

    dx

--- js/intro.js
+++ js/intro.js
@@ -5,7 +5,13 @@ introscreen.style.display = 'none';
 formula.style.display = 'none';
 function play()
 {
-    
+    var priverA = new Rythm()
+
+      priverA.setMusic('/assets/ost/bg.mp3')
+
+
+      priverA.setGain(1.0);
+      priverA.start()
     var introscreen = document.getElementById('sequencer')
     var formula = document.getElementById('dolam')
     var preintro = document.getElementById('enter')
