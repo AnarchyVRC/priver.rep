62ef98ac16bf0268d43ecef64d0681886c2adf00
Skullface/Tarkov <106857740+terkoshi@users.noreply.github.com>
2023-02-10 13:18:09 -0500

    d

--- js/intro.js
+++ js/intro.js
@@ -19,8 +19,8 @@ function play()
     priverA.setMusic('/assets/ost/bg.mp3')
     priverA.setGain(1.0);
     priverA.addRythm('pulsinator', 'pulse', 275, 355, {
-        min: 10,
-        max: 40
+        min: 5,
+        max: 12
      })
      priverA.addRythm('pulsinator', 'color', 0, 10, {
         from: [0,0,255],
